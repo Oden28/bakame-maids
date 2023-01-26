2750b0fd8cdebd71c11c74cd2a1c9a37f20d269c
Joshu Bukuru <controls083@gmail.com>
2023-01-26 23:22:23 +0200

    50% done with maids screen

---
+++ src/components/Content/Maids/index.jsx
@@ -0,0 +1,98 @@
+import React from "react";
+import Maid from "./Maid";
+import SearchBar from "./SearchBar";
+
+const Maids = () => {
+  return (
+    <div className="container p-6">
+      <SearchBar />
+      <div className="bg-gray rounded-[20px] p-6 mb-10 max-h-[700px] overflow-y-auto">
+        <Maid maid_info={maids[0]} />
+        <Maid maid_info={maids[0]} />
+
+        <div>Button</div>
+      </div>
+    </div>
+  );
+};
+
+export default Maids;
+
+// maid data
+const maids = [
+  {
+    id: 1,
+    name: "Didie Mucyo",
+    gender: "male",
+    image:
+      "https://femalebodyguards.files.wordpress.com/2020/07/istock-627909282-1514234385.jpg",
+    age: 23,
+    reviews: 5,
+    review_count: 50,
+    avg_salary: 200000,
+    open_negotiation: true,
+    location: "Kigali, Rwanda",
+    description:
+      "I’m a big boy living in a big city and ready to work my face off! I’ve worked...Read more",
+  },
+  {
+    id: 2,
+    name: "Didie Mucyo",
+    gender: "male",
+    image:
+      "https://femalebodyguards.files.wordpress.com/2020/07/istock-627909282-1514234385.jpg",
+    age: 23,
+    reviews: 5,
+    review_count: 50,
+    avg_salary: 200000,
+    open_negotiation: true,
+    location: "Kigali, Rwanda",
+    description:
+      "I’m a big boy living in a big city and ready to work my face off! I’ve worked...Read more",
+  },
+  {
+    id: 3,
+    name: "Didie Mucyo",
+    gender: "male",
+    image:
+      "https://femalebodyguards.files.wordpress.com/2020/07/istock-627909282-1514234385.jpg",
+    age: 23,
+    reviews: 5,
+    review_count: 50,
+    avg_salary: 200000,
+    open_negotiation: true,
+    location: "Kigali, Rwanda",
+    description:
+      "I’m a big boy living in a big city and ready to work my face off! I’ve worked...Read more",
+  },
+  {
+    id: 4,
+    name: "Didie Mucyo",
+    gender: "male",
+    image:
+      "https://femalebodyguards.files.wordpress.com/2020/07/istock-627909282-1514234385.jpg",
+    age: 23,
+    reviews: 5,
+    review_count: 50,
+    avg_salary: 200000,
+    open_negotiation: true,
+    location: "Kigali, Rwanda",
+    description:
+      "I’m a big boy living in a big city and ready to work my face off! I’ve worked...Read more",
+  },
+  {
+    id: 5,
+    name: "Didie Mucyo",
+    gender: "male",
+    image:
+      "https://femalebodyguards.files.wordpress.com/2020/07/istock-627909282-1514234385.jpg",
+    age: 23,
+    reviews: 5,
+    review_count: 50,
+    avg_salary: 200000,
+    open_negotiation: true,
+    location: "Kigali, Rwanda",
+    description:
+      "I’m a big boy living in a big city and ready to work my face off! I’ve worked...Read more",
+  },
+];
--- src/components/Content/index.jsx
+++ src/components/Content/index.jsx
@@ -1,10 +1,15 @@
 import React from "react";
 import HomePage from "./HomePage";
+import Maids from "./Maids";
+import { Route, Routes } from "react-router-dom";
 
 const Content = () => {
   return (
     <div>
-      <HomePage />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/maids" element={<Maids />} />
+      </Routes>
     </div>
   );
 };
